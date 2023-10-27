72d27c700017adb51d8e37c062862bed78b7144b
Kill3r1322 <doraszymo73@gmail.com>
2023-10-27 17:37:26 +0200

    sterowanie

--- skrypty.js
+++ skrypty.js
@@ -1,34 +1,65 @@
-$(document).keydown(function (e) {
+// the array to add pressed keys to
+var keys = [];
+// listen for which key is pressed
+document.addEventListener('keydown', (event) => {
+    if ($.inArray(event.key, keys) == -1) {
+        keys.push(event.key);
+    }
+    console.log('keys array after pressed = ' + keys);
+});
+// listen for which key is unpressed
+document.addEventListener('keyup', (event) => {
+	// the key to remove
+	var removeKey = event.key;
+  // rmeove it
+	keys = $.grep(keys, function(value) {
+		return value != removeKey;
+	});
+	console.log('keys array after unpress = ' + keys);
+});
 
-	var key = e.which;
-	if (key == "83") {
-		$('#player1').animate(
-			{ "bottom": "-=4px" },
-			{
-				duration: 10,
-			})
-	}
-	if (key == "87") {
-		$('#player1').animate(
-			{ "bottom": "+=4px" },
-			{
-				duration: 10,
-			})
-	}
-    if (key == "40") {
-		$('#player2').animate(
-			{ "bottom": "-=4px" },
-			{
-				duration: 10,
-			})
-	}
-	if (key == "38") {
-		$('#player2').animate(
-			{ "bottom": "+=4px" },
-			{
-				duration: 10,
-			})
-	}
+const duration = 10
+
+$(document).keydown(function () {
+    if (keys.includes("ArrowUp")) {
+        $('#player1').animate(
+            { "bottom": "+=4px" },
+            {
+                duration,
+            })
+    }
+
+    if (keys.includes("ArrowDown")) {
+        $('#player1').animate(
+            { "bottom": "-=4px" },
+            {
+                duration,
+            })
+    }
+
+    if (keys.includes("w")) {
+        $('#player2').animate(
+            { "bottom": "+=4px" },
+            {
+                duration,
+            })
+    }
+
+    if (keys.includes("s")) {
+        $('#player2').animate(
+            { "bottom": "-=4px" },
+            {
+                duration,
+            })
+    }
 });
 
 //pilka
+
+const ball = document.getElementById("ball");
+const degree = 45;
+$(function () {
+    setInterval(function () {
+        
+    }, 1000);
+});
